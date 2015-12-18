26235432f57d0bd48acfe637a3f519c3fe9f2063
jeanturgeon2015 <fotojt@gmail.com>
2015-12-18 18:40:58 -0500

    remodelage des pages produits-phares

--- javascript/directory.js
+++ javascript/directory.js
@@ -1,26 +1,27 @@
 $(document).ready(function() {
 	$('table').dataTable({"language": {
-    "decimal":        "",
-    "emptyTable":     "Aucun employé enregistré",
-    "info":           "Employés _START_ à _END_ sur _TOTAL_ employés",
-    "infoEmpty":      "Employés 0 à 0 sur 0 employés",
-    "infoFiltered":   "(filtered from _MAX_ total entries)",
-    "infoPostFix":    "",
-    "thousands":      ",",
-    "lengthMenu":     "Présenter _MENU_ employées",
-    "loadingRecords": "Chargement...",
-    "processing":     "En traitement...",
-    "search":         "Chercher:",
-    "zeroRecords":    "Aucune entrée correspondante trouvée",
-    "paginate": {
-        "first":      "Premier",
-        "last":       "Dernier",
-        "next":       "Suivant",
-        "previous":   "Précédent"
-    },
-    "aria": {
-        "sortAscending":  ": Trier en ordre croissant",
-        "sortDescending": ": Trier en ordre décroissant"
-    }
-}});
+		    "decimal":        "",
+		    "emptyTable":     "Aucun employé enregistré",
+		    "info":           "Employés _START_ à _END_ sur _TOTAL_ employés",
+		    "infoEmpty":      "Employés 0 à 0 sur 0 employés",
+		    "infoFiltered":   "(filtered from _MAX_ total entries)",
+		    "infoPostFix":    "",
+		    "thousands":      ",",
+		    "lengthMenu":     "Présenter _MENU_ employées",
+		    "loadingRecords": "Chargement...",
+		    "processing":     "En traitement...",
+		    "search":         "Chercher:",
+		    "zeroRecords":    "Aucune entrée correspondante trouvée",
+		    "paginate": {
+		        "first":      "Premier",
+		        "last":       "Dernier",
+		        "next":       "Suivant",
+		        "previous":   "Précédent"
+		    },
+		    "aria": {
+		        "sortAscending":  ": Trier en ordre croissant",
+		        "sortDescending": ": Trier en ordre décroissant"
+		    }
+		}
+	});
 });
