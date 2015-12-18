f46457e69d1292e2c058d6fb3018d10f08efbbdc
jeanturgeon2015 <fotojt@gmail.com>
2015-12-18 13:22:53 -0500

    introduction du controle de la connection (Admin)

--- javascript/connection.js
+++ javascript/connection.js
@@ -1,3 +1,33 @@
-$(document).onload() {
+$(document).ready(function() {
 	
+	$("#connect").click(function() {
+		if (sessionActive) {
+			open("../html/forms.html");
+		} else {
+			open("../html/connection.html");
+		}
+	});
+});
+function infoConnection() {			
+	localStorage.setItem("password", "Inf2005");	
+	localStorage.setItem("userName", "admin");	
 }
+
+function validate() {
+	var sessionActive = false;
+	
+	var adminPassword = localStorage.getItem("password").toString();
+	var adminUser = localStorage.getItem("userName").toString();	
+	var userName = document.getElementById("user").value;
+	var password = document.getElementById("pass").value;	
+	
+	if (userName == adminUser && password == adminPassword) {
+		sessionActive = true;		
+		open("../html/forms.html");
+	} else {
+		alert("Le mot de passe et/ou le nom d'usager est incorrect. Veuillez réessayer");
+	}
+};
+
+	
+
