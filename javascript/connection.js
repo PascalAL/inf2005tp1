ba1eec9892723111b63413ac01cc030fd706f0ea
jeanturgeon2015 <fotojt@gmail.com>
2015-12-18 15:37:01 -0500

    finalisation du procesus d'athentification et changement du logo sur Bottin et Connection

--- javascript/connection.js
+++ javascript/connection.js
@@ -1,33 +1,38 @@
-$(document).ready(function() {
+function session() {	/* vérifie si l'utilisateur s'est déjà authentifié*/
+	active = sessionStorage.getItem("sessionActive");
 	
-	$("#connect").click(function() {
-		if (sessionActive) {
-			open("../html/forms.html");
-		} else {
-			open("../html/connection.html");
-		}
-	});
-});
-function infoConnection() {			
-	localStorage.setItem("password", "Inf2005");	
-	localStorage.setItem("userName", "admin");	
+	if (active) {
+		
+		open("../html/forms.html", "_self");
+		
+	} else {
+		open("../html/connection.html",  "_self");
+	}	
 }
 
-function validate() {
-	var sessionActive = false;
-	
+function validate() {	/*valide le nom d'usager et le mot de passe' */
 	var adminPassword = localStorage.getItem("password").toString();
 	var adminUser = localStorage.getItem("userName").toString();	
 	var userName = document.getElementById("user").value;
 	var password = document.getElementById("pass").value;	
 	
 	if (userName == adminUser && password == adminPassword) {
-		sessionActive = true;		
-		open("../html/forms.html");
+		sessionStorage.setItem("sessionActive", true);		
+	
+		open("../html/forms.html", "_self");
+	
 	} else {
 		alert("Le mot de passe et/ou le nom d'usager est incorrect. Veuillez réessayer");
 	}
-};
+}
+
+function infoConnection() {	  /* sauvegarde les infos d'administrateur */		
+	localStorage.setItem("password", "Inf2005");	
+	localStorage.setItem("userName", "admin");
+}
+
+
+
 
 	
 
