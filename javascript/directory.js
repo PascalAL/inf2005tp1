41dd1865e95a7e44b28dae3182566aaff73f3825
PascalAL <p_a-l@hotmail.com>
2015-12-17 19:12:43 -0500

    Changements positionnement logo et paramètres langues datatables

--- javascript/directory.js
+++ javascript/directory.js
@@ -1,3 +1,26 @@
 $(document).ready(function() {
-	$('table').dataTable();
+	$('table').dataTable({"language": {
+    "decimal":        "",
+    "emptyTable":     "No data available in table",
+    "info":           "Showing _START_ to _END_ of _TOTAL_ entries",
+    "infoEmpty":      "Showing 0 to 0 of 0 entries",
+    "infoFiltered":   "(filtered from _MAX_ total entries)",
+    "infoPostFix":    "",
+    "thousands":      ",",
+    "lengthMenu":     "Show _MENU_ entries",
+    "loadingRecords": "Loading...",
+    "processing":     "Processing...",
+    "search":         "Search:",
+    "zeroRecords":    "No matching records found",
+    "paginate": {
+        "first":      "First",
+        "last":       "Last",
+        "next":       "Next",
+        "previous":   "Précédent"
+    },
+    "aria": {
+        "sortAscending":  ": activate to sort column ascending",
+        "sortDescending": ": activate to sort column descending"
+    }
+}});
 });
