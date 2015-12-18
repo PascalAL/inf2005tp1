545a5ed0854435dca56af3ca3e68d55b0e9713fe
PascalAL <p_a-l@hotmail.com>
2015-12-17 20:16:30 -0500

    Ajustement CSS

---
+++ html/footer.php
@@ -0,0 +1,4 @@
+<footer>
+	<div>Ce site est développé par <a href="mailto:pascalaubelarouche@voyagesxperience.com?subject=Voyages Xpérience">Pascal Aubé-Larouche</a> et <a href="mailto:jeanturgeon@voyagesxperience.com?subject=Voyages Xpérience">Jean Turgeon</a> dans le cadre du cours 
+	<a href="http://www.etudier.uqam.ca/cours?sigle=INF2005">INF2005</a> à <a href="http://www.uqam.ca">l'UQAM</a></div>
+</footer>
