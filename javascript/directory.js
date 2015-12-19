869fa04c7965f4ec909bbf34179c89fcb0cb18b6
PascalAL <p_a-l@hotmail.com>
2015-12-18 20:27:16 -0500

    Ajustement de la BD dans le localStorage

--- javascript/directory.js
+++ javascript/directory.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+	
+	alert("Pascal");
+loadTableContent();
+
 	$('table').dataTable({"language": {
     "decimal":        "",
     "emptyTable":     "Aucun employé enregistré",
@@ -23,4 +27,22 @@ $(document).ready(function() {
         "sortDescending": ": Trier en ordre décroissant"
     }
 }});
+
+
 });
+
+
+
+function loadTableContent() {
+var dbJSON = localStorage.getItem('db');
+if(dbJSON != null){
+	var db = JSON.parse(dbJSON);
+	var employeesList = db["employees"];
+	var count = employeesList.length;
+	alert(count);
+	
+	for(var i = 0; i < count; i++){
+		$('table tr:last').after('<tr><td><a href="mailto:' + employeesList[i].email + '">' + employeesList[i].firstName +' '+ employeesList[i].lastName +'</a></td><td>' + employeesList[i].empFunction + '</td><td>' + employeesList[i].phone + '</td></tr>');
+	} 
+}
+}
--- javascript/forms.js
+++ javascript/forms.js
@@ -2,6 +2,25 @@ $(document).ready(function() {
 
 }); 
 
+var db = null;
+var regionJSONObject;
+
+function product(){
+	this.supplier;
+	this.name;
+	this.description;
+	this.price;
+	this.image;
+}
+
+function employee(){
+	this.firstName;
+	this.lastName;
+	this.email;
+	this.empFunction;
+	this.phone;
+}
+
 function submitProduct(){
 	
 	var supplier = document.getElementById("productSupplier").value;
@@ -11,5 +30,83 @@ function submitProduct(){
 	var price = document.getElementById("productPrice").value;
 	var image = document.getElementById("productImage").value;
 	
+	initialiseDBFromLocalStorage();
+	initialiseRegionProductsArray(region);
 	
+	var productJSON = new product();
+
+	productJSON.supplier = supplier;
+	productJSON.name = name;
+	productJSON.description = description;
+	productJSON.price = price;
+	productJSON.image = image;
+
+	regionJSONObject.push(productJSON);
+	db[region] = regionJSONObject;
+	localStorage.setItem('db', JSON.stringify(db));
+
+	$('#resetProduct').trigger('click');
+	var div = document.getElementById('productSubmitResult');
+	div.innerHTML = "Produit Enregistré.";
+	return false;
 };
+
+function submitEmployee(){
+	
+	var lastName = document.getElementById("employeeLastName").value;
+	var firstName = document.getElementById("employeeFirstName").value;
+	var email = document.getElementById("employeeEmail").value;
+	var empFunction = document.getElementById("employeeFunction").value;
+	var phone = document.getElementById("employeePhone").value;
+	
+	initialiseDBFromLocalStorage();
+
+	employeesJSONObject = db["employees"];
+	if(employeesJSONObject == null){
+		("La région était null");
+		employeesJSONObject = [];
+	}
+
+	var employeeJSON = new employee();
+
+	employeeJSON.lastName = lastName;
+	employeeJSON.firstName = firstName;
+	employeeJSON.email = email;
+	employeeJSON.empFunction = empFunction;
+	employeeJSON.phone = phone;
+
+	employeesJSONObject.push(employeeJSON);
+
+	db["employees"] = employeesJSONObject;
+
+	localStorage.setItem('db', JSON.stringify(db));
+
+	$('#resetEmployee').trigger('click');
+	var div = document.getElementById('employeeSubmitResult');
+	div.innerHTML = "Employé Enregistré.";
+	return false;
+};
+
+
+
+
+
+function initialiseDBFromLocalStorage(){
+	db = localStorage.getItem('db');
+	if(db == null){
+		db = {"africa": [], "asia": [], "europe": [], "middleEast": [], "southAmerica": [], "southPacific": [], "employees": []};		
+		localStorage.setItem('db', db);
+	}else{
+		db = JSON.parse(db);
+	}
+}
+
+function initialiseRegionProductsArray(region){
+	regionJSONObject = db[region];
+	if(regionJSONObject == null){
+		regionJSONObject = [];
+	}
+}
+
+
+
